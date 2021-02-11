f0163dc9a68035c838d24a0c44a7265944b8deec
HamzhSuilik <9hamzh@gmail.com>
2021-02-11 20:22:14 +0200

    ffdd

--- app.js
+++ app.js
@@ -151,21 +151,52 @@ function multiplyArray(multArr) {
 
 /////////////////////////////////////
 /* STRETCH GOAL: Problem 6
-Write a function called multiplyAnyArray() that takes an array of numbers of any length as its argument and returns an array whose first element is the product of those numbers, and the second element is a string that EXACTLY follows this example and concatenates a message using the arguments that were passed into the function:
+Write a function called multiplyAnyArray() that takes an array of numbers of any length as its argument and returns an array 
+whose 
+first element is
+ the product of those numbers,
+  and the second element
+   is a string that EXACTLY follows this example and 
+concatenates a message using the arguments that were passed into the function:
 
 "The numbers 1,2,3,4,5 have a product of 120."
 
-IMPORTANT DETAIL: You may not use the arithmetic operator * in this function. To do multiplication, use your multiply() function that you've already created. You're going to have to be resourceful to figure out how to do this. However, you may continue to use the + operator for string concatenation.
+IMPORTANT DETAIL: You may not use the arithmetic operator * in this function. To do multiplication, use your multiply() function 
+that you've already created. You're going to have to be resourceful to figure out how to do this. However, you may continue 
+to use the + operator for string concatenation.
 
 This function should be dynamic, accepting an array of any length.
 
-Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiplyAnyArray() function and see if the test passes.*/
+Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for 
+the testMultiplyAnyArray() function and see if the test passes.*/
 
 // Write your code here
 let testDynamicArray = [1, 2, 3, 4, 5]; //eslint-disable-line
 
+function multiply(array){
+  let mult =1;
+  for (let index = 0; index < array.length; index++) {
+    mult = mult * array[index];
+  }
+  return mult;
+}
+
+function get_text(multArr,mult){
+  let text = 'The numbers ';
+  for (let index = 0; index < (multArr.length-1); index++) {
+    text =text + multArr[index];
+    text =text +',';
+  }
+  text =text + multArr[multArr.length-1];
+  text =text +' have a product of ';
+  text =text + mult +'.';
+  return text;
+}
+
 function multiplyAnyArray(dynamicArray) {
-  //eslint-disable-line
+  let mult= multiply(dynamicArray);
+
+  return[mult,get_text(dynamicArray,mult)];
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
